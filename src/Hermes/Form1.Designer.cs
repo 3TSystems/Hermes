fe568de72c81c6e06651e702aca75692340119cf
Jordan Hiebert <hiebert@gmail.com>
2013-05-08 17:38:46 -0500

    v0.2
    - Allow custom base location via dropdown. This will also allow for
    additional base locations in the future.
    - Fix latitude limits

--- src/Hermes/Form1.Designer.cs
+++ src/Hermes/Form1.Designer.cs
@@ -30,18 +30,20 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -49,40 +51,23 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(249, 131);
+            this.groupBox1.Size = new System.Drawing.Size(249, 148);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Tower Location";
             // 
-            // textBox1
-            // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(92, 41);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(133, 25);
-            this.textBox1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Enabled = false;
-            this.label1.Location = new System.Drawing.Point(20, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Latitude";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Enabled = false;
-            this.label2.Location = new System.Drawing.Point(20, 75);
+            this.label2.Location = new System.Drawing.Point(20, 100);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 17);
             this.label2.TabIndex = 3;
@@ -91,10 +76,27 @@
             // textBox2
             // 
             this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(92, 72);
+            this.textBox2.Location = new System.Drawing.Point(92, 97);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(133, 25);
             this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Latitude";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(92, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 25);
+            this.textBox1.TabIndex = 0;
             // 
             // groupBox2
             // 
@@ -102,9 +104,9 @@
             this.groupBox2.Controls.Add(this.textBox3);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.textBox4);
-            this.groupBox2.Location = new System.Drawing.Point(12, 149);
+            this.groupBox2.Location = new System.Drawing.Point(12, 174);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(249, 131);
+            this.groupBox2.Size = new System.Drawing.Size(249, 120);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Remote Location";
@@ -112,7 +114,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 75);
+            this.label3.Location = new System.Drawing.Point(20, 72);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 17);
             this.label3.TabIndex = 3;
@@ -120,7 +122,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(92, 72);
+            this.textBox3.Location = new System.Drawing.Point(92, 69);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(133, 25);
             this.textBox3.TabIndex = 2;
@@ -128,7 +130,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 44);
+            this.label4.Location = new System.Drawing.Point(20, 41);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(54, 17);
             this.label4.TabIndex = 1;
@@ -136,7 +138,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(92, 41);
+            this.textBox4.Location = new System.Drawing.Point(92, 38);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(133, 25);
             this.textBox4.TabIndex = 0;
@@ -144,23 +146,12 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.label5);
-            this.groupBox3.Location = new System.Drawing.Point(12, 286);
+            this.groupBox3.Location = new System.Drawing.Point(12, 300);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(249, 102);
             this.groupBox3.TabIndex = 5;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Result";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(35, 416);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(202, 35);
-            this.button1.TabIndex = 0;
-            this.button1.TabStop = false;
-            this.button1.Text = "Calculate Distance";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label5
             // 
@@ -172,12 +163,45 @@
             this.label5.Text = "...";
             this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 35);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "Calculate Distance";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Niverville",
+            "(Custom)"});
+            this.comboBox1.Location = new System.Drawing.Point(92, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(133, 25);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Preset";
+            // 
             // Form1
             // 
             this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 479);
+            this.ClientSize = new System.Drawing.Size(273, 496);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
@@ -215,6 +239,8 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
 
     }
 }
--- src/Hermes/Form1.cs
+++ src/Hermes/Form1.cs
@@ -14,14 +14,13 @@ namespace Hermes
 {
     public partial class Form1 : Form
     {
-        GeoCoordinate baseCoord = new GeoCoordinate(49.606950, -97.048447);
+        GeoCoordinate baseCoord = new GeoCoordinate();
 
         public Form1()
         {
             InitializeComponent();
 
-            textBox1.Text = baseCoord.Latitude.ToString();
-            textBox2.Text = baseCoord.Longitude.ToString();
+            comboBox1.SelectedIndex = 0;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,10 +31,12 @@ namespace Hermes
         private void button1_Click(object sender, EventArgs e)
         {
             GeoCoordinate remoteCoord = new GeoCoordinate();
-            double remoteLat, remoteLong, calcDist;
+            double baseLat, baseLong, remoteLat, remoteLong, calcDist;
 
             try
             {
+                baseLat = Convert.ToDouble(textBox1.Text);
+                baseLong = Convert.ToDouble(textBox2.Text);
                 remoteLat = Convert.ToDouble(textBox4.Text);
                 remoteLong = Convert.ToDouble(textBox3.Text);
             }
@@ -46,13 +47,15 @@ namespace Hermes
                 return;
             }
 
-            if (Math.Abs(remoteLat) > 180 || Math.Abs(remoteLong) > 180)
+            if (Math.Abs(remoteLat) > 90 || Math.Abs(remoteLong) > 180 || Math.Abs(baseLat) > 90 || Math.Abs(baseLong) > 180)
             {
                 label5.ForeColor = System.Drawing.Color.Black;
                 label5.Text = "Error 2";
                 return;
             }
 
+            baseCoord.Latitude = baseLat;
+            baseCoord.Longitude = baseLong;
             remoteCoord.Latitude = remoteLat;
             remoteCoord.Longitude = remoteLong;
 
@@ -63,6 +66,36 @@ namespace Hermes
             else label5.ForeColor = System.Drawing.Color.DarkRed;
 
             label5.Text = calcDist + " km";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Niverville":
+                    baseCoord.Latitude = 49.606950;
+                    baseCoord.Longitude = -97.048447;
+                    break;
+                case "(Custom)":
+                    baseCoord.Latitude = 0;
+                    baseCoord.Longitude = 0;
+                    break;
+            }
+
+            if (comboBox1.SelectedItem.ToString().Equals("(Custom)"))
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+            }
+            else
+            {
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox1.Text = baseCoord.Latitude.ToString();
+                textBox2.Text = baseCoord.Longitude.ToString();
+            }
         }
     }
 }
